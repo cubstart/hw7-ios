8f9e46309764db9ca4e3073b2075d627c384a42e
justinwongeecs <jwonghelloworld@berkeley.edu>
2023-10-23 13:51:43 -0700

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Justin Wong on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
